14a8fa45ac4ce93a844ddbb41c9462b39e53bb92
suivue <suivue@gmail.com>
2025-06-09 17:25:18 +0530

    Update minstalldebu.sh: Fix apt sources.list not populating

--- minstalldebu.sh
+++ minstalldebu.sh
@@ -7,24 +7,23 @@
 
 sleep 10
 
-# Once the minimal base system is installed chroot into it and install
-# kernel and bootloader as a minimum.
 export PS1="(chroot) \u@\h:\w # "
 export DEBIAN_FRONTEND=noninteractive
 
 # Add/update sources.list entry
+#
 distro=$(cat /etc/os-release | grep '^ID=' | cut -d"=" -f2)
 relname=$(cat /etc/os-release | grep 'CODENAME' | cut -d"=" -f2)
 loopN=$(losetup | awk 'NR==2 { print $1}' | cut -d'/' -f3)
 
-if [ "$distro" == "debian" ]
+if [[ "$distro" == "debian" ]]
 then
 cat << EOF > /etc/apt/sources.list
 deb http://deb.debian.org/debian $relname main contrib non-free non-free-firmware
 deb http://security.debian.org/debian-security $relname-security main contrib non-free non-free-firmware
 deb http://deb.debian.org/debian $relname-updates main contrib non-free non-free-firmware
 EOF
-elif [ "$distro" == "ubuntu" ]
+elif [[ "$distro" == "ubuntu" ]]
 then
 cat << EOF > /etc/apt/sources.list
 deb http://archive.ubuntu.com/ubuntu/ $relname main restricted universe multiverse
@@ -35,38 +34,44 @@ fi
 
 # optional if proxy has to be setup
 # echo 'Acquire::http::Proxy "http://192.168.3.222:8000";' > /etc/apt/apt.conf.d/99proxy
+
+# Refresh apt database and upgrade system before proceding.
+#
 apt-get update
 apt-get dist-upgrade
 
-
 # Make sure you write '/etc/fstab' otherwise system will not boot later.
+#
 echo "UUID=$(blkid /dev/mapper/${loopN}p2 -s UUID -o value) / ext4 noatime 0 1" > /etc/fstab
 echo "UUID=$(blkid /dev/mapper/${loopN}p1 -s UUID -o value) /boot/efi vfat umask=0077 0 1" >> /etc/fstab
 
 # Install linux kernel and bootloader as a minimum.
-apt-get install linux-image-generic grub2 -y
-grub-install # does not require device name to be passed 
+# 'grub2' package will install 'grub-pc' by default. So install 'grub-efi'
+# For efi version, grub-install does not require device name to be passed
+#
+apt-get install linux-image-generic grub-efi -y
+grub-install
 update-grub
 
 # Install any other software you desire to have on a minimal environment.
 #
 apt-get install bash-completion file nano network-manager -y
-# Desktop-environment if needed; There should be a '^' at the end as per
-# instruction on the referenced source.
-#apt-get install ubuntu-mate-desktop^
 
 # If using NetworkManager, following entry is required so that it manages
 # all the devices.
+#
 cat << EOF > /etc/NetworkManager/conf.d/01-nm-manage.conf
 [keyfile]
 unmanaged-devices=none
 EOF
 
+echo $relname > /etc/hostname
+
 # passwd for root
+#
 printf "\nSetting password for root\n"
 passwd
 
-echo $relname > /etc/hostname
 # Add user for reqular use.
 #useradd -m -G adm,cdrom,sudo,dip,plugdev -s /bin/bash $distro
 #passwd $distro
